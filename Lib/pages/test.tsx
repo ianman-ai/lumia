38578c728ca45cc3c90d4540b75e1a447398cd1d
ianman-ai <i.jritchie@hotmail.co.uk>
2025-07-20 02:48:47 +0100

    Update test.tsx

--- Lib/pages/test.tsx
+++ Lib/pages/test.tsx
@@ -1,1 +1,29 @@
 
+'use client';
+import { useEffect, useState } from 'react';
+import { supabase } from '@/lib/supabaseClient';
+
+export default function TestPage() {
+  const [blogs, setBlogs] = useState<any[]>([]);
+
+  useEffect(() => {
+    async function fetchBlogs() {
+      const { data, error } = await supabase.from('blogs').select('*');
+      if (error) console.error(error);
+      else setBlogs(data);
+    }
+
+    fetchBlogs();
+  }, []);
+
+  return (
+    <div>
+      <h1>Blogs</h1>
+      <ul>
+        {blogs.map(blog => (
+          <li key={blog.id}>{blog.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
